d139c07984ed38c6f99d4c85726ccf4e8891f634
AMAN7040 <amansingh79620@gmail.com>
2024-12-09 19:37:47 +0530

    role slice reducer added

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from './usersSlice';
+import roleReducer from './roleSlice';
 
 const appstore  = configureStore({
     reducer: {
         users: usersReducer,
+        roles: roleReducer,
       },
 });
 
