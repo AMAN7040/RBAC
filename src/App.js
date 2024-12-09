301ebdd5ee085e9380df002274ad0039ca62085d
AMAN7040 <amansingh79620@gmail.com>
2024-12-09 19:45:28 +0530

    root fie updated to add tostify

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Dashboard from "./pages/Users";
 import Roles from "./pages/Roles";
-import Permissions from "./pages/Permissions";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+
 
 const App = () => {
   return (
     <Router>
       <div className="flex">
         <div className="flex-1 min-h-screen">
-          <Header /> 
+          <Header />
+          <ToastContainer />
           <main className="p-6">
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/roles" element={<Roles />} />
-              <Route path="/permissions" element={<Permissions />} />
             </Routes>
           </main>
         </div>
